2b1650da714d163e2030621c411b2f631454715e
Folkes <folkes@spotify-clone.local>
2026-09-12 12:52:54 +0200

    fix: font paths, shadcn token mapping

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -3,12 +3,12 @@ import localFont from "next/font/local";
 export const spotifyMixUI = localFont({
   src: [
     {
-      path: "./fonts/SpotifyMixUI-Regular.woff2",
+      path: "../app/fonts/SpotifyMixUI-Regular.woff2",
       weight: "400",
       style: "normal",
     },
     {
-      path: "./fonts/SpotifyMixUI-Bold.woff2",
+      path: "../app/fonts/SpotifyMixUI-Bold.woff2",
       weight: "700",
       style: "normal",
     },
@@ -18,14 +18,14 @@ export const spotifyMixUI = localFont({
 });
 
 export const spotifyMixTitle = localFont({
-  src: "./fonts/SpotifyMixUITitleVariable.woff2",
+  src: "../app/fonts/SpotifyMixUITitleVariable.woff2",
   variable: "--font-spotify-mix-title",
   display: "swap",
   weight: "400 900",
 });
 
 export const spotifyMixMono = localFont({
-  src: "./fonts/SpotifyMixMono-Regular.woff2",
+  src: "../app/fonts/SpotifyMixMono-Regular.woff2",
   variable: "--font-spotify-mix-mono",
   display: "swap",
   weight: "400",
